0a91b7f98fe165dda4710a5d8a81b00c41cb24d4
Yuri Pagani <yuripagani@gmail.com>
2021-10-30 11:26:38 -0300

    defining templates

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,8 +1,20 @@
 """
 To render html views
 """
+import random
 from django.http import HttpResponse
+from django.template.loader import render_to_string
+from articles.models import Article
 
 def home_view(request):
-    html_str = """<h1>Hello Bro</h1>"""
+    rand_id = random.randint(1, 4)
+    article_obj = Article.objects.get(id=rand_id)
+    
+    context = {
+        "title": article_obj.title,
+        "content": article_obj.content,
+        "id": article_obj.id,
+    }
+    
+    html_str = render_to_string("home_view.html", context=context)
     return HttpResponse(html_str)
