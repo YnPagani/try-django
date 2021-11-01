88330d5a75a298ca0d5b519ae5d656a7607002f3
Yuri Pagani <yuripagani@gmail.com>
2021-11-01 18:08:27 -0300

    added: formatted list with the articles in the home page

--- trydjango/views.py
+++ trydjango/views.py
@@ -9,11 +9,13 @@ from articles.models import Article
 def home_view(request):
     rand_id = random.randint(1, 4)
     article_obj = Article.objects.get(id=rand_id)
+    article_query_set = Article.objects.all()
     
     context = {
         "title": article_obj.title,
         "content": article_obj.content,
         "id": article_obj.id,
+        "article_list": article_query_set
     }
     
     html_str = render_to_string("home_view.html", context=context)
