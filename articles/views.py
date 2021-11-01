f5529cbcd9710a349633cd5faec8dd5cf173ce1e
Yuri Pagani <yuripagani@gmail.com>
2021-11-01 18:43:44 -0300

    added: dynamic url for articles details

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from articles.models import Article
+
 # Create your views here.
+def article_detail_view(request, id=None):
+    if id is None:
+        article_obj = None
+    else:
+        article_obj = Article.objects.get(id=id)
+    context = {
+        "object": article_obj,
+    }
+    
+    return render(request, "articles/detail.html", context=context)
