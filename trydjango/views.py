7c1292f728e295fb61c6d5709a90449d43102c5c
Yuri Pagani <yuripagani@gmail.com>
2021-11-06 19:51:56 -0300

    flake8

--- trydjango/views.py
+++ trydjango/views.py
@@ -6,17 +6,17 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 from articles.models import Article
 
+
 def home_view(request):
     rand_id = random.randint(1, 4)
     article_obj = Article.objects.get(id=rand_id)
     article_query_set = Article.objects.all()
-    
     context = {
         "title": article_obj.title,
         "content": article_obj.content,
         "id": article_obj.id,
         "article_list": article_query_set
     }
-    
+
     html_str = render_to_string("home_view.html", context=context)
     return HttpResponse(html_str)
