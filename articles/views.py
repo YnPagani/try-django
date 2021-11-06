f95b77b0577118784f3dfc1e3c715366b01c4e29
Yuri Pagani <yuripagani@gmail.com>
2021-11-06 19:51:08 -0300

    added a way to create articles through a view

--- articles/views.py
+++ articles/views.py
@@ -2,7 +2,37 @@ from django.shortcuts import render
 
 from articles.models import Article
 
+
 # Create your views here.
+def article_search_view(request):
+    query_dict = request.GET
+    query = query_dict.get("q")     # input name defined in base.html
+
+    if query is not None:
+        article_obj = Article.objects.get(id=query)
+    else:
+        article_obj = None
+
+    context = {
+        "object": article_obj
+    }
+
+    return render(request, "articles/search.html", context=context)
+
+
+def article_create_view(request):
+    context = {}
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+
+        new_article = Article.objects.create(title=title, content=content)
+        context["new_article"] = new_article
+        context["created"] = True
+
+    return render(request, "articles/create.html", context=context)
+
+
 def article_detail_view(request, id=None):
     if id is None:
         article_obj = None
@@ -11,5 +41,5 @@ def article_detail_view(request, id=None):
     context = {
         "object": article_obj,
     }
-    
+
     return render(request, "articles/detail.html", context=context)
